b1805ec9f3c539b91c546e5e7e3baf628bf9b3ef
preshy <110857483+preshiy@users.noreply.github.com>
2026-05-30 18:34:07 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ export default function App() {
     setResult(null);
 
     try {
-      const res = await fetch("http://localhost:3001/analyze", {
+      const res = await fetch("https://tokenomiq-backend.onrender.com/analyze", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ tokenQuery, whitepaperText }),
